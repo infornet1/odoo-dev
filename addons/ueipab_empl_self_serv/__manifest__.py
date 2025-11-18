18ebb3551f7b3d97ad60361abf4942809b320e00
infornet1 <infornet1@ueipab.com>
2025-11-18 18:58:26 -0400

    feat(hr): Initial scaffolding for ueipab_empl_self_serv module

---
+++ addons/ueipab_empl_self_serv/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'UEIPAB Employee Self-Service',
+    'version': '17.0.1.0.0',
+    'summary': 'A portal for employees to manage their own information and payslips.',
+    'author': '3DVision C.A.',
+    'website': 'https://www.3dvision.com.ve',
+    'license': 'AGPL-3',
+    'category': 'Human Resources/Payroll',
+    'depends': [
+        'hr',
+        'hr_contract',
+        'portal',
+        'hr_payroll_community',
+    ],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
